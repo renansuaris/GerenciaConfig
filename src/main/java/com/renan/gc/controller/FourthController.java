cb485205bb3b0d4d117ca217faeae608a1733a10
Renan <renanalencar313@gmail.com>
2024-11-05 14:58:05 -0300

    Commit

--- src/main/java/com/renan/gc/controller/FourthController.java
+++ src/main/java/com/renan/gc/controller/FourthController.java
@@ -1,4 +1,16 @@
 package com.renan.gc.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class FourthController {
+
+    @GetMapping
+    public String controller() {
+        return "Está FUncionando.";
+    }
+
 }
